7f61a62260f4f50f615941327e5111200790a931
SahilBoda07 <sahilboda07@gmail.com>
2024-07-18 19:55:59 +0530

    All File uploaded

---
+++ assets/js/lazyload.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll('.lazy-load');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    lazyImages.forEach(image => {
+        observer.observe(image);
+    });
+});
